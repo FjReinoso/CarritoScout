1b958716f5c3d9e03f2eb08f43776b2d03cdbec9
Francisco José García Reinoso <ifjreinoso@gmail.com>
2025-06-13 13:11:50 +0200

    Configuracion admin
    Adaptamos lo modelos para que pueda cambiarlos el admin

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Supermercado, Producto, Precio
 
-# Register your models here.
+@admin.register(Supermercado)
+class SupermercadoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'direccion', 'geolocalizacion')
+    search_fields = ('nombre', 'direccion')
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'categoria', 'unidad_medida', 'precio')
+    search_fields = ('nombre', 'categoria')
+    list_filter = ('categoria',)
+
+@admin.register(Precio)
+class PrecioAdmin(admin.ModelAdmin):
+    list_display = ('id_producto', 'id_supermercado', 'precio', 'fecha_actualizacion')
+    search_fields = ('id_producto__nombre', 'id_supermercado__nombre')
+    list_filter = ('id_supermercado',)
--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import PerfilUsuario
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 @admin.register(PerfilUsuario)
 class PerfilUsuarioAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'direccion', 'telefono', 'fecha_nacimiento')
     search_fields = ('usuario__username', 'usuario__email', 'direccion', 'telefono')
+
+# Registrar el modelo User para manipulación directa desde el admin
+admin.site.unregister(User)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
